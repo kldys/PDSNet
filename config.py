258e375995fa2a43f490e3e8c17f945b4186d5b8
kldys <133568287+kldys@users.noreply.github.com>
2023-06-30 09:52:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+class DefaultConfig(object):
+
+    data_root = '......'
+    num_data = 45000
+    crop_size = 128
+    noise_level =2
+
+    batch_size =16
+    use_gpu = True
+    num_workers = 1  
+
+    max_epoch = 50
+    lr = 0.0005 
+    lr_decay = 0.5
+
+    load_model_path = '......'
+    save_model_path = '......'
+
+opt = DefaultConfig()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
